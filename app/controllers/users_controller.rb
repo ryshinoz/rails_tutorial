8058d0969e37d3a88e1bfe4fce4ea0bec74d3ec5
Ryosuke Shinozaki <ryshinoz@gmail.com>
2016-02-17 19:05:15 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -21,9 +22,6 @@ class UsersController < ApplicationController
       @user.send_activation_email
       flash[:info] = 'Please check your email to activate your account'
       redirect_to root_url
-      # log_in @user
-      # flash[:success] = 'Welcome to the Sample App'
-      # redirect_to @user
     else
       render 'new'
     end
@@ -53,14 +51,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in'
-      redirect_to login_url
-    end
   end
 
   def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_many :microposts, dependent: :destroy
+
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -56,6 +58,10 @@ class User < ActiveRecord::Base
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where('user_id = ?', id)
   end
 
   private
